a4e0895fb1b1f93b81cfcc7f79ab681bc15c147f
fabianosalles <fabiano.salles@gmail.com>
2026-04-09 01:45:01 -0300

    feat - shema updates (early stages)

--- db/estrutura incial.sql
+++ db/estrutura incial.sql
@@ -5,6 +5,21 @@
     2. popula com dados iniciais
  */
 
+/*  
+   USUÁRIOS
+*/
+
+create table usuarios (
+    id       integer primary key autoincrement, 
+    usuario  text not null,
+    senha    text not null,
+    ativo    integer
+);
+
+
+/*  
+   CARGOS
+*/
 create table cargos (
     id    integer primary key autoincrement,
     nome  text not null
@@ -14,15 +29,63 @@ create table cargos (
 insert into cargos (nome) values ('Recepcionista');
 insert into cargos (nome) values ('Gerente');
 
+/*  
+   FUNCIONÁRIOS
+*/
 create table funcionarios (
-    id       integer primary key autoincrement,
-    nome     text not null,
-    id_cargo integer not null,
-    ativo    integer,
+    id          integer primary key autoincrement,
+    nome        text not null,
+    id_cargo    integer not null,
+    id_usuario  integer,
+    ativo       integer,
 
-    foreign key(id_cargo) references cargos(id)
+    foreign key(id_cargo) references cargos(id),
+    foreign key(id_usuario) references usuarios(id)
 );
 
 insert into funcionarios (nome, id_cargo, ativo) values ('Cristiane Barros', 1, 1);
 
 
+
+/*  
+   CLIENTES
+*/
+create table clientes(
+    id          integer primary key autoincrement,
+    nome        text not null,
+    celular     text,
+    contato     text,
+    cadastro    text  /* ISO8601 strings ("YYYY-MM-DD HH:MM:SS.SSS") */
+);
+
+
+/*  
+   CONTRATOS
+   ------   
+   todo: levantar os atributos do contrado
+*/
+create table contratos (
+    id           integer primary key autoincrement
+);
+
+
+/*  
+   CLIENTES
+   ------
+   tipo (C = carro, M = moto)
+*/
+create table veiculos (
+    id           integer primary key autoincrement,
+    tipo         text(1) not null check( tipo in ('C', 'M') ),
+    marca        text,
+    modelo       text,
+    ano          integer,
+    placa        text not null,
+    cadastro     text,  /* ISO8601 strings ("YYYY-MM-DD HH:MM:SS.SSS") */
+
+    id_cliente   integer,
+    id_contrato  integer,
+
+    foreign key(id_cliente) references clientes(id),
+    foreign key(id_contrato) references contratos(id)
+);
